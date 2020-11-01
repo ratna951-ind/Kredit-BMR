ec2615d451949c63a37befc03a5f8a56edc712a3
Anak Agung Bagus Sugiastika Putra <bagoes.goenk12@gmail.com>
2020-11-01 19:58:34 +0800

    Angsuran
    - Pembuatan Seeder & Tabel Angsuran (1 dari 3 halaman)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Kios;
 use App\User;
 use App\Peran;
@@ -16,9 +17,28 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $data['kios'] = Kios::where('aktif','1')->count();
-        $data['user'] = User::where('aktif','1')->whereNotIn('peran_id', [1])->count();
-        $data['peran'] = Peran::count();
+        $role = Auth::user()->role_id;
+        
+        if($role == 1){
+            $data['kios'] = Kios::where('aktif','1')->count();
+            $data['user'] = User::where('aktif','1')->whereNotIn('peran_id', [1])->count();
+            $data['peran'] = Peran::count();
+        }
+        else if($role == 2){
+            
+        }
+        else if($role == 3){
+            
+        }
+        else if($role == 4){
+            
+        }
+        else if($role == 5){
+            
+        }
+        else if($role == 6){
+            
+        }
 
         return view('home', $data);
     }
---
+++ database/migrations/2020_11_01_065417_create_angsuran_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAngsuranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('angsuran', function (Blueprint $table) {
+            $table->increments('pinjaman');
+            $table->integer('bln_6');
+            $table->integer('bln_12');
+            $table->integer('bln_18');
+            $table->integer('bln_24');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('angsuran');
+    }
+}
